87d688577076b58048f9baa876c982536356f8ec
Ömer Serfice <srfcomr@gmail.com>
2021-10-22 22:26:14 +0300

    validation yapısı kuruldu.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Ad Soyad Kısmı Boş Geçilemez!");
+            RuleFor(x => x.WriterEmail).NotEmpty().WithMessage("Mail Adresi Boş Geçilemez");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre Boş Geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen En az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen En fazla 50 karakter giriniz ");
+        }
+    }
+}
